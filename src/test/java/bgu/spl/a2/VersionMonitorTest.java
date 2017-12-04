b3f5a9fe016e118ea3d060a440ba4ab7dddecb2b
myteaisgreen <34246628+myteaisgreen@users.noreply.github.com>
2017-12-04 22:14:26 +0200

    Update VersionMonitorTest.java

--- src/test/java/bgu/spl/a2/VersionMonitorTest.java
+++ src/test/java/bgu/spl/a2/VersionMonitorTest.java
@@ -6,28 +6,37 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class VersionMonitorTest {
+import junit.framework.Assert;
 
+public class VersionMonitorTest {
+	VersionMonitor verMonitor;
 	@Before
 	public void setUp() throws Exception {
+		verMonitor = new VersionMonitor();
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		verMonitor = null;
+		assertNull(verMonitor);
 	}
 
 	@Test
 	public void testGetVersion() {
-		fail("Not yet implemented");
+		Assert.assertEquals("The initial value should be 0", 0, verMonitor.getVersion());
+		verMonitor.inc();
+		Assert.assertEquals("The initial value should be 1", 1, verMonitor.getVersion());
 	}
 
 	@Test
 	public void testInc() {
-		fail("Not yet implemented");
+		verMonitor.inc();
+		Assert.assertEquals("The initial value should be 1", 1, verMonitor.getVersion());
 	}
 
 	@Test
 	public void testAwait() {
+		
 		fail("Not yet implemented");
 	}
 
