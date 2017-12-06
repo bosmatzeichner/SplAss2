279391bce4140cdcfc4e603f1cef97ef1d2649c1
myteaisgreen <34246628+myteaisgreen@users.noreply.github.com>
2017-12-06 21:06:42 +0200

    Update VersionMonitorTest.java

--- src/test/java/bgu/spl/a2/VersionMonitorTest.java
+++ src/test/java/bgu/spl/a2/VersionMonitorTest.java
@@ -10,6 +10,7 @@ import junit.framework.Assert;
 
 public class VersionMonitorTest {
 	VersionMonitor verMonitor;
+
 	@Before
 	public void setUp() throws Exception {
 		verMonitor = new VersionMonitor();
@@ -21,23 +22,42 @@ public class VersionMonitorTest {
 		assertNull(verMonitor);
 	}
 
-	@Test
-	public void testGetVersion() {
-		Assert.assertEquals("The initial value should be 0", 0, verMonitor.getVersion());
-		verMonitor.inc();
-		Assert.assertEquals("The initial value should be 1", 1, verMonitor.getVersion());
-	}
-
-	@Test
-	public void testInc() {
-		verMonitor.inc();
-		Assert.assertEquals("The initial value should be 1", 1, verMonitor.getVersion());
-	}
-
+	/*
+	 * @Test public void testGetVersion() {
+	 * Assert.assertEquals("The initial value should be 0", 0,
+	 * verMonitor.getVersion()); verMonitor.inc();
+	 * Assert.assertEquals("The initial value should be 1", 1,
+	 * verMonitor.getVersion()); }
+	 * 
+	 * @Test public void testInc() { verMonitor.inc();
+	 * Assert.assertEquals("The initial value should be 1", 1,
+	 * verMonitor.getVersion()); }
+	 */
 	@Test
 	public void testAwait() {
-		
-		fail("Not yet implemented");
+		final boolean[] testArray = new boolean[1];
+		// System.out.println(testArray[0]);
+		Thread testThread = new Thread(new Runnable() {
+			public void run() {
+				try {
+					verMonitor.await(0);
+				} catch (InterruptedException e) {
+					// testArray[0] = true;
+					System.out.println("!!! " + testArray[0]);
+				}
+				testArray[0] = true;
+
+			}
+		});
+
+		testThread.start();
+		verMonitor.inc();
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+		}
+
+		assertEquals(true, testArray[0]);
 	}
 
 }
