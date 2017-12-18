6b3a9d5b48be56dbac6c02acfb325fc73f59a8d8
myteaisgreen <34246628+myteaisgreen@users.noreply.github.com>
2017-12-18 23:22:53 +0200

    Update Action.java

--- src/main/java/bgu/spl/a2/Action.java
+++ src/main/java/bgu/spl/a2/Action.java
@@ -1,135 +1,126 @@
 package bgu.spl.a2;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
- * represents an actor thread pool - to understand what this class does please
- * refer to your assignment.
+ * an abstract class that represents an action that may be executed using the
+ * {@link ActorThreadPool}
  *
  * Note for implementors: you may add methods and synchronize any of the
  * existing methods in this class *BUT* you must be able to explain why the
- * synchronization is needed. In addition, the methods you add can only be
- * private, protected or package protected - in other words, no new public
- * methods
+ * synchronization is needed. In addition, the methods you add to this class can
+ * only be private!!!
+ *
+ * @param <R> the action result type
  */
-public class ActorThreadPool {
+public abstract class Action<R> {
+	private ActorThreadPool actorThreadPool;
+	private Promise<R> myPromise = new Promise<R>();
+	private callback myCallback;
+	private AtomicReference<String> actionName;
+	protected String ownerActorName;
+	protected PrivateState ownerActorState;
 	
-	int numOfThreads;
-	boolean isShutDown = false;
-
-	LinkedHashMap<String, ConcurrentLinkedQueue<Action>> qsOfActors;
-	LinkedHashMap<String, PrivateState> privateStatesOfActors;
-	Thread[] threads;
-	VersionMonitor vMonitor;
-
-
 	/**
-	 * creates a {@link ActorThreadPool} which has nthreads. Note, threads
-	 * should not get started until calling to the {@link #start()} method.
-	 *
-	 * Implementors note: you may not add other constructors to this class nor
-	 * you allowed to add any other parameter to this constructor - changing
-	 * this may cause automatic tests to fail..
-	 *
-	 * @param nthreads
-	 *            the number of threads that should be started by this thread
-	 *            pool
-	 */
-	public ActorThreadPool(int nthreads) {
-		numOfThreads = nthreads;
-
-		qsOfActors = new LinkedHashMap<>();
-		privateStatesOfActors = new LinkedHashMap<>();
-
-		threads = new Thread[numOfThreads];
-
-		for (Thread i : threads){
-			i = new Thread(() -> {
-				while(!isShutDown){
-					for(Map.Entry<String, ConcurrentLinkedQueue<Action>> j : qsOfActors.entrySet()){
-						ConcurrentLinkedQueue<Action> queueOfAnActor = (ConcurrentLinkedQueue<Action>) j.getValue();
-						if(!queueOfAnActor.isEmpty()){
-							Action action = queueOfAnActor.poll();
-							vMonitor.inc();
-							PrivateState tempPriState = privateStatesOfActors.get(j.getKey());
-							action.handle(this, j.getKey(), tempPriState);
-							tempPriState.addRecord(action.getActionName());
-						}
-					}
-					try {
-						vMonitor.await(vMonitor.getVersion());
-					} catch (InterruptedException e) {}
+     * start handling the action - note that this method is protected, a thread
+     * cannot call it directly.
+     */
+    protected abstract void start();
+    
+    /**
+    *
+    * start/continue handling the action
+    *
+    * this method should be called in order to start this action
+    * or continue its execution in the case where it has been already started.
+    *
+    * IMPORTANT: this method is package protected, i.e., only classes inside
+    * the same package can access it - you should *not* change it to
+    * public/private/protected
+    *
+    */
+   /*package*/ final void handle(ActorThreadPool pool, String actorId, PrivateState actorState) {
+	   if(myCallback == null){
+		   actorThreadPool = pool;
+		   ownerActorName = actorId;
+		   ownerActorState = actorState;
+		   start();
+	   }
+	   else {
+		   myCallback.call();
+	   }
+   }
+    
+    /**
+     * add a callback to be executed once *all* the given actions results are
+     * resolved
+     * 
+     * Implementors note: make sure that the callback is running only once when
+     * all the given actions completed.
+     *
+     * @param actions
+     * @param callback the callback to execute once all the results are resolved
+     */
+    protected final void then(Collection<? extends Action<?>> actions, callback callback) {
+       	AtomicInteger numOfActions = new AtomicInteger(actions.size());
+       	myCallback = callback;
+       	for (Action<?> action : actions) {
+			action.getResult().subscribe(()->{
+				if(numOfActions.decrementAndGet() == 0){
+					actorThreadPool.qsOfActors.get(ownerActorName).add(this);
 				}
 			});
-		}
-	}
+		}  	       	
+    }
 
-
-	/**
-	 * getter for actors
-	 * @return actors
-	 */
-	public Map<String, PrivateState> getActors(){
-		return privateStatesOfActors;
+    /**
+     * resolve the internal result - should be called by the action derivative
+     * once it is done.
+     *
+     * @param result - the action calculated result
+     */
+    protected final void complete(R result) {
+       	myPromise.resolve(result);
+    }
+    
+    /**
+     * @return action's promise (result)
+     */
+    public final Promise<R> getResult() {
+    	return myPromise;
+    }
+    
+    /**
+     * Send an action to another actor
+     * 
+     * @param action
+     * 				the action
+     * @param actorId
+     * 				actor's id
+     * @param actorState
+	 * 				actor's private state (actor's information)
+	 *    
+     * @return promise that will hold the result of the sent action
+     */
+	public Promise<?> sendMessage(Action<?> action, String actorId, PrivateState actorState){
+        actorThreadPool.submit(action, actorId, actorState);
+        return getResult();
 	}
 	
 	/**
-	 * getter for actor's private state
-	 * @param actorId actor's id
-	 * @return actor's private state
+	 * set action's name
+	 * @param actionName
 	 */
-	public PrivateState getPrivateState(String actorId){
-		return privateStatesOfActors.get(actorId);
+	public void setActionName(String actionName){
+		this.actionName.getAndSet(actionName);
 	}
 	
 	/**
-	 * submits an action into an actor to be executed by a thread belongs to
-	 * this thread pool
-	 *
-	 * @param action
-	 *            the action to execute
-	 * @param actorId
-	 *            corresponding actor's id
-	 * @param actorState
-	 *            actor's private state (actor's information)
+	 * @return action's name
 	 */
-	public void submit(Action<?> action, String actorId, PrivateState actorState) {
-		if (!isShutDown){
-			if(!qsOfActors.containsKey(actorId)){
-				qsOfActors.put(actorId, new ConcurrentLinkedQueue<>());
-				privateStatesOfActors.put(actorId, actorState);
-			}
-			qsOfActors.get(actorId).add(action);
-			vMonitor.inc();
-		}
+	public String getActionName(){
+        return actionName.get();
 	}
-		
-	/**
-	 * closes the thread pool - this method interrupts all the threads and waits
-	 * for them to stop - it is returns *only* when there are no live threads in
-	 * the queue.
-	 *
-	 * after calling this method - one should not use the queue anymore.
-	 *
-	 * @throws InterruptedException
-	 *             if the thread that shut down the threads is interrupted
-	 */
-	public void shutdown() throws InterruptedException {
-		isShutDown = true;
-		for(Thread i : threads){
-			i.interrupt();
-		}
-	}
-
-	/**
-	 * start the threads belongs to this thread pool
-	 */
-	public void start() {
-		for(Thread i : threads){
-			i.start();
-		}
-	}
-
 }
